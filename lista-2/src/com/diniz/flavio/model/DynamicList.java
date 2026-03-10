83c2ba71797e1df4c9ab62686693bde4029f1f7b
Flávio Diniz de Sousa <102635915+Diniz-s-flavio@users.noreply.github.com>
2026-03-10 10:29:31 -0300

    Feat: lista dinamica

--- lista-2/src/com/diniz/flavio/model/DynamicList.java
+++ lista-2/src/com/diniz/flavio/model/DynamicList.java
@@ -29,6 +29,89 @@ public class DynamicList {
 
     }
 
+    public void addOrdered(Object item) {
+        Node newNode = new Node(item);
+        Node current = this.head;
+        while (current.hasNext() && (Integer) current.getNext().getItem() < (Integer) item) {
+            current = current.getNext();
+        }
+
+        newNode.setNext(current);
+        if (!newNode.hasNext()) {
+            this.tail = newNode;
+        }
+    }
+
+    public Object removeFirst() {
+        this.validateEmpty();
+
+        Node current = this.head;
+
+        Node previous = this.head.getNext();
+
+        Object returnedItem = previous.getItem();
+
+        current.setNext(previous.getNext());
+
+        if (!current.hasNext()) {
+            this.tail = current;
+        }
+
+        return returnedItem;
+    }
+
+    public Object removeLast() {
+        this.validateEmpty();
+
+        Object returnedItem = this.tail.getItem();
+        Node current = this.head;
+
+        while (current.hasNext() && current.getNext() != this.tail ) {
+            current = current.getNext();
+        }
+
+        current.setNext(null);
+        this.tail = current;
+
+        return returnedItem;
+    }
+
+
+    public Object remove(Object item) {
+        this.validateEmpty();
+
+        Node current = this.head;
+
+        while (current.hasNext() && !current.getNext().getItem().equals(item)) {
+            current = current.getNext();
+        }
+
+        if (!current.hasNext()) {
+            return null;
+        }
+
+        Node q = current.getNext();
+        Object returnedItem = q.getItem();
+        current.setNext(q.getNext());
+
+        if (!current.hasNext()) {
+            this.tail = current;
+        }
+
+        return returnedItem;
+    }
+
+    public Object pesquisa (Object chave) {
+        this.validateEmpty();
+        Node current = head;
+        while (current.getNext() != null ) {
+            if (current.getNext().getItem().equals(chave))
+                return current.getNext().getItem();
+            current = current.getNext();
+        }
+        return null;
+    }
+
     public void print() {
         Node current = this.head;
         while (current != null) {
@@ -39,6 +122,12 @@ public class DynamicList {
 
     public boolean isEmpty() {
         return this.head == this.tail;
+    }
+
+    public void validateEmpty() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("A lista está vazia.");
+        }
     }
 
 
